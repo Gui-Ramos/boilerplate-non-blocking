01cc3856ddb81203a53e0b849ef8edecc4ae5f7e
GuilhermeSantosHypery <guilherme.santos@hypery.io>
2024-09-21 14:18:32 -0300

    First commit

---
+++ src/main/java/com/non_blocking/non_blocking/NonBlockingIoApplication.java
@@ -0,0 +1,13 @@
+package com.non_blocking.non_blocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonBlockingIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonBlockingIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/non_blocking/non_blocking/NonBlockingIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.non_blocking.non_blocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonBlockingIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
